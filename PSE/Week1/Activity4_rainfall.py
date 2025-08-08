03674cb01c86d78839f7d270614b67c9bb5aba97
DodIlagan-cloud <isaganimeer@gmail.com>
2025-08-09 11:17:54 +1200

    week2

--- PSE/Week1/Activity4_rainfall.py
+++ PSE/Week1/Activity4_rainfall.py
@@ -17,12 +17,17 @@ if __name__ == "__main__":
     averf = np.round(np.mean(rainfall_list),2)
     day0arr = np.where(rainfall_list == 0)[0]
     day0 = day0arr.size
-    #daya5 = np.where(rainfall_list > 5)[0]
+    daya5 = np.where(rainfall_list > 5)[0]
     dayname = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'] 
-    daya5 = [dayname for dayname, rain_amt in zip(dayname, rainfall_list) if rain_amt > 5] 
+    #for index in daya5:
+    #    print("this",index)
+    #    print(f'\t ({daya5[index]} mm)')
+    #    print(f'\t{dayname[index]} ({daya5[index]} mm)')
+    daya5 = [daymap for daymap, rain_amt in zip(dayname, rainfall_list) if rain_amt > 5] 
 
     print("RainFall for the week:",rainfall_list)
     print("Total Rainfall for the week:",sumrf," mm")
     print("Average Rainfall for the week",averf," mm")
     print("Number of days that has no rain:",day0," days")
     print("Number of days that has rainfall more than 5:",daya5)
+
