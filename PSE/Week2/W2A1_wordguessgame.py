447638997a9e045e3b74399dfbc2333e23a5f269
DodIlagan-cloud <isaganimeer@gmail.com>
2025-08-09 12:12:17 +1200

    Update W2A1_wordguessgame.py

--- PSE/Week2/W2A1_wordguessgame.py
+++ PSE/Week2/W2A1_wordguessgame.py
@@ -1,1 +1,30 @@
-W2A1_wordguessgame
+#W2A1_wordguessgame
+import random as rdm
+import numpy as np 
+
+def gen_word ():
+    word_list = ["braveheart","hugo","nosferatu","interstellar","kingdom","annihilation","speed"]
+    guess_word = rdm.choice(word_list)
+    return guess_word
+
+def gen_blank ():
+    word_blank_arr =np.empty(shape=g_word_size,dtype='<U1')
+    return word_blank_arr
+
+def play ():
+    trial = 0
+    while trial > 3:
+        guess_letter = input()
+        trial ++
+
+if __name__ == "__main__":
+    g_guess_word=gen_word()
+    g_word_size = len(g_guess_word)
+    print(f"{g_guess_word},{g_word_size}")
+    print(f"Guess the random word, it has {g_word_size} letters.")
+    blanks_arr=gen_blank()
+    #blanks_arr=[_]
+    print(blanks_arr)
+
+
+    g_guess_letter = input()
