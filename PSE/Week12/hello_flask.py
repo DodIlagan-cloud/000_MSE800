e77354f3f580efec4d8ffb6290339ec2278ca43a
DodIlagan-cloud <isaganimeer@gmail.com>
2025-10-18 11:19:03 +1300

    update

--- PSE/Week12/hello_flask.py
+++ PSE/Week12/hello_flask.py
@@ -1,5 +1,12 @@
-from flask import Flask 
-app = Flask(__name__) 
-@app.route("/") 
-def hello_flask(): 
+"""W12 A1 - Flask test"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_flask():
+    """Flask test"""
     return "<p>Hello, Flask!</p>"
+
+if __name__ == '__main__':
+    app.run (debug=True)
